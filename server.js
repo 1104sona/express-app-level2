10f0b1edd3198414a928d19955e4f2ccaf98efe1
1104sona panda <sonali.pnd04@gmail.com>
2024-09-16 09:52:46 +0000

    connect MongoDB

--- server.js
+++ server.js
@@ -1,79 +1,21 @@
-const express = require("express")
-const mongoose = require('mongoose');
+const express = require('express');
+const connectDB = require('./config/db');
 
-const app = express()
-const port = 3000
 
-//connect to mongoose
-mongoose.connect('mongodb://localhost/myDatabase', { useNewUrlParser:true, useUnifiedTopology: true});
+const app = express();
+const PORT = process.env.PORT || 3000;
 
-const db = mongoose.connection
-db.on("error", console
-    .error.blind(console, "Mongodb connection error:"))
-db.once("open",() => {
-    console.log ("connected to mongodb successfully")
-})
-app.get("/", (req,res) => {
-    res.send("hello world")
-});
+// Connect to MongoDB
+connectDB();
 
-let books = []
-//get all books
-app.get("/books",(req,res) => {
-    res.json(books);
-});
+app.use(express.json());
 
-//create a book
-app.post("/books",(req,res) => {
-    //logic to add a book
-    const { title, author } = req.body;
-  if (!title || !author) {
-    return res.status(400).send('Missing title or author');
-  }
-  const newBook = { id: books.length + 1, title, author };
-  books.push(newBook);
-  res.status(201).send(newBook);
+app.get("/", (req, res) => {
+    res.send("HomePage Of The App");
 });
 
 
-//get a single book
-app.get("/books/:id", (req, res) => {
-    const book = books.find(b => {b.id === PerformanceObserverEntryList(req.params.id) })
 
-    if( !book)  {
-        return res.status(404).send("books not found") 
-       }
-
-       res.json(book)
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
 });
-
-
-//update a book
-app.put("/books/:id",(req,res) => {
-    const book = books.find(b => { b.id === parselnt(req.params.id) })
-
-    if (book) {
-        return res.status(404).send("book not found")
-    }
-    const { title, author } = req.body;
-  books.title = title || book.title;
-  books.author = author || book.author;
-
-  res.send(book)
-});
-
-//delete a book
-app.delete("/books/:id",(req,res) => {
-   //logic to delete a book
-   const bookIndex = books.findIndex(b => b.id === parseInt(req.params.id));
-   if (bookIndex === -1) {
-     return res.status(404).send('Book not found');
-   }
- 
-   books.splice(bookIndex, 1);
-   res.status(204).send();
-})
-
-app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
-  });
